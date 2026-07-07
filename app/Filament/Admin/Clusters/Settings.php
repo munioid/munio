fdc70e159f792df169fbed0c93687bc8d1b75d4f
aneksa <andyeka07@gmail.com>
2026-07-07 17:36:05 +0700

    fix: remove settings navigation group

--- app/Filament/Admin/Clusters/Settings.php
+++ app/Filament/Admin/Clusters/Settings.php
@@ -4,11 +4,8 @@ namespace App\Filament\Admin\Clusters;
 
 use BackedEnum;
 use Filament\Clusters\Cluster;
-use UnitEnum;
 
 class Settings extends Cluster
 {
     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-squares-2x2';
-
-    protected static UnitEnum|string|null $navigationGroup = 'Settings';
 }
