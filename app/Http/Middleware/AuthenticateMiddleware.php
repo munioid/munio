78e6cf8985c7c5babf42468053ec189d32985d50
aneksa <andyeka07@gmail.com>
2026-07-12 21:10:55 +0700

    fix: change middleware class name

--- app/Http/Middleware/AuthenticateMiddleware.php
+++ app/Http/Middleware/AuthenticateMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+class AuthenticateMiddleware extends Middleware
 {
     protected function redirectTo(Request $request): ?string
     {
