70c2c223871ab2c4b48377451b619d4b79ca65bc
aneksa <andyeka07@gmail.com>
2026-08-13 16:58:50 +0700

    Revert "style: enlarge Events & Reservations widget headings to text-xl"
    This reverts commit beaba8e19cc90f9851719ae96a3739b33630887f.
    
    Co-authored-by: multica-agent <github@multica.ai>

--- app/Filament/Admin/Widgets/ReservationsOverviewWidget.php
+++ app/Filament/Admin/Widgets/ReservationsOverviewWidget.php
@@ -4,22 +4,14 @@ namespace App\Filament\Admin\Widgets;
 
 use App\Enums\ReservationStatusEnum;
 use App\Models\Event\Reservation;
-use Filament\Schemas\Components\Component;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Illuminate\Support\HtmlString;
 
 class ReservationsOverviewWidget extends BaseWidget
 {
     protected ?string $heading = 'Reservations';
 
     protected static ?int $sort = 12;
-
-    public function getSectionContentComponent(): Component
-    {
-        return parent::getSectionContentComponent()
-            ->heading(new HtmlString('<span class="text-xl">'.e($this->getHeading()).'</span>'));
-    }
 
     protected function getStats(): array
     {
