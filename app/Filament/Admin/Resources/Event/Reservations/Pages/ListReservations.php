db5b1e83781abe55542a326295fa201eb005cc31
aneksa <andyeka07@gmail.com>
2026-08-20 10:28:27 +0700

    chore(agent): uncommitted changes from task

--- app/Filament/Admin/Resources/Event/Reservations/Pages/ListReservations.php
+++ app/Filament/Admin/Resources/Event/Reservations/Pages/ListReservations.php
@@ -8,6 +8,7 @@ use Filament\Actions\CreateAction;
 use Filament\Actions\ExportAction;
 use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Icons\Heroicon;
 
 class ListReservations extends ListRecords
 {
@@ -19,6 +20,9 @@ class ListReservations extends ListRecords
             CreateAction::make(),
             ExportAction::make()
                 ->exporter(ReservationExporter::class)
+                ->label('Export')
+                ->icon(Heroicon::ArrowDownTray)
+                ->color('info')
                 ->options([
                     'organization_id' => Filament::getTenant()?->id,
                 ]),
